2ce126492efddc0f2915825dddbefaaa6d79663c
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-19 15:21:55 +0530

    intial commit

---
+++ Problems/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package Problems.SnakeAndLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("welcome to the game of snake and ladders");
+    }
+}
+
+
+
+
+
