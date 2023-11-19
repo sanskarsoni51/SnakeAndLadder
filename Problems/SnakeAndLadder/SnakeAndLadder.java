a827742cada2f8525e33c143fb2768c13df5f10d
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-19 21:37:16 +0530

    UC2 adding dice to the game

--- Problems/SnakeAndLadder/SnakeAndLadder.java
+++ Problems/SnakeAndLadder/SnakeAndLadder.java
@@ -1,6 +1,12 @@
 package Problems.SnakeAndLadder;
 
 public class SnakeAndLadder {
+
+    // now we make a function for Dice number
+    static int Dice(){
+        int roll = (int) (Math.random()*100) % 6 + 1;
+        return roll;
+    }
     public static void main(String[] args) {
         // welcome message
         System.out.println("welcome to the game of snake and ladders");
@@ -8,6 +14,10 @@ public class SnakeAndLadder {
         // adding player one
         int Pos_Player1 = 0;
         System.out.println("welcome player one. Your starting position will be " + Pos_Player1);
+
+        // now we will call the function of the dice 
+        int dice = Dice();
+        
     }
 }
 
